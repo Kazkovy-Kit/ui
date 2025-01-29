f3e67cce84032421d85f697f8f2c8883ff5670bc
Vladyslav G. <hello@mikield.rocks>
2025-01-29 22:17:38 +0100

    fix

--- components/ThemeSelect.vue
+++ components/ThemeSelect.vue
@@ -17,11 +17,13 @@ function nextTheme() {
 
 <template>
   <Button variant="outline" v-bind="$attrs" @click="nextTheme" class="min-w-[50px]">
-    <Icon name="lucide:sun"
-          v-show="colorMode.preference === 'light'"
-          class="h-[1.2rem] w-[1.2rem] transition-opacity ease-linear"/>
-    <Icon name="lucide:moon"
-          v-show="colorMode.preference === 'dark'"
-          class="h-[1.2rem] w-[1.2rem] transition-opacity ease-linear"/>
+    <client-only>
+      <Icon name="lucide:sun"
+            v-show="colorMode.preference === 'light'"
+            class="h-[1.2rem] w-[1.2rem] transition-opacity ease-linear"/>
+      <Icon name="lucide:moon"
+            v-show="colorMode.preference === 'dark'"
+            class="h-[1.2rem] w-[1.2rem] transition-opacity ease-linear"/>
+    </client-only>
   </Button>
 </template>
